69913237bf4055579a084dadc82033e59dd6416e
Ignacio Olaciregui <ignacioola@gmail.com>
2022-10-25 20:50:21 -0300

    Fix code styling

--- src/helpers.ts
+++ src/helpers.ts
@@ -1,27 +1,19 @@
-import { BigNumber } from "ethers";
-import { formatUnits } from "ethers/lib/utils";
+import { BigNumber } from 'ethers'
+import { formatUnits } from 'ethers/lib/utils'
 
-export const formatBigNumber = (
-  number?: BigNumber,
-  displayDecimals = 18,
-  decimals = 18
-) => {
+export const formatBigNumber = (number?: BigNumber, displayDecimals = 18, decimals = 18) => {
   if (!number) {
-    return;
+    return
   }
 
-  const remainder = number.mod(
-    BigNumber.from(10).pow(decimals - displayDecimals)
-  );
-  return formatUnits(number.sub(remainder), decimals);
-};
+  const remainder = number.mod(BigNumber.from(10).pow(decimals - displayDecimals))
+  return formatUnits(number.sub(remainder), decimals)
+}
 
 export const foramtAddress = (address?: string) => {
   if (!address) {
-    return;
+    return
   }
 
-  return `${address.substring(0, 4)}...${address.substring(
-    address.length - 4
-  )}`;
-};
+  return `${address.substring(0, 4)}...${address.substring(address.length - 4)}`
+}
